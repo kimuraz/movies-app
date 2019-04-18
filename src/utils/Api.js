89f749cad47cf5710807bb27923feb8668a10c30
Kimura <joseduardo.kimura@gmail.com>
2019-04-18 00:05:22 -0300

    :bulb: Add comments endpoints to Api utils

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -21,8 +21,13 @@ Api.interceptors.request.use(config => {
 
 export const doLogin = credentials => Api.post('auth', credentials);
 export const getMovies = () => Api.get('movies');
+export const getMovieById = id => Api.get(`movies/${id}`);
 export const newMovie = (movie) => Api.post('movies', movie);
 export const editMovie = (id, movie) => Api.patch(`movies/${id}`, movie);
 export const deleteMovie = (id) => Api.delete(`movies/${id}`);
+export const getMovieComments = id => Api.get(`movies/${id}/comments`);
+export const newComment = comment => Api.post(`comments`, comment);
+export const editComment = comment => Api.patch(`comments/${comment.id}`, comment);
+export const deleteComment = (id) => Api.delet(`comments/${id}`);
 
 export default Api;
